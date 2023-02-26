5f2700ce420ba882dbef10b0e88f30c05a6d0de0
BramMeerten <bram.mee@gmail.com>
2023-02-26 12:02:07 +0100

    Webpack / typescript / react setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = 'style-loader';
+
+const config = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {test: /\.(ts|tsx)$/i, loader: 'ts-loader', exclude: ['/node_modules/']},
+            {test: /\.css$/i, use: [stylesHandler,'css-loader']},
+            {test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i, type: 'asset'},
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    },
+};
+
+module.exports = () => {
+    config.mode = isProduction ? 'production' : 'development';
+    return config;
+};
